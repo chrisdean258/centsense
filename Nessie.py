5ef57a0730d6f5ecdf78026397e033e0217e8544
chrisdean258 <chrisdean258@gmail.com>
2017-11-04 17:33:03 -0400

    Added get all for customers

--- Nessie.py
+++ Nessie.py
@@ -7,7 +7,8 @@ class Customer:
     def __init__(self, ID):
         url = 'http://api.reimaginebanking.com/customers/{}?key={}'.format(ID,apiKey)
         person = requests.get(url)
-        if(person.status_code == 404):
+        self.good = True;
+        if(person.status_code // 100 == 4):
             self.good = False
             return;
         json_data = json.loads(person.text);
@@ -20,7 +21,33 @@ class Customer:
         self.city = json_data["address"]["city"]
         self.street_name = json_data["address"]["street_name"]
 
+    def __init(self):
+        self.good = True
+
+    @staticmethod
+    def get_all():
+        customers = []
+        url = "http://api.reimaginebanking.com/customers?key={}".format(apiKey)
+        people = requests.get(url)
+        if(people.status_code // 400 == 4):
+            return []
+        json_data = json.loads(people.text)
+        for i, person in enumerate(json_data):
+            newPerson = Customer;
+            newPerson.ID = json_data[i]["_id"]
+            newPerson.first_name = json_data[i]["first_name"]
+            newPerson.last_name = json_data[i]["last_name"]
+            newPerson.zip = json_data[i]["address"]["zip"]
+            newPerson.street_number = json_data[i]["address"]["street_number"]
+            newPerson.state = json_data[i]["address"]["state"]
+            newPerson.city = json_data[i]["address"]["city"]
+            newPerson.street_name = json_data[i]["address"]["street_name"]
+            customers.append(newPerson)
+        print(customers)
+        return customers
 
 
-ID = "59fe1e3cb390353c953a1c2c"
-Customer(ID)
+
+
+
+Customer.get_all()
