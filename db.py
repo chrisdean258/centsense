cd5780cdbd6df2a51c5dbc67885b2d63461a50a0
Ubuntu <ubuntu@ip-172-31-28-69.us-east-2.compute.internal>
2017-11-05 05:20:20 +0000

    make db.py a function

--- db.py
+++ db.py
@@ -1,20 +1,27 @@
 #!/usr/bin/python
 import MySQLdb
 
-db = MySQLdb.connect(host="localhost",    # your host, usually localhost
-                     user="root",         # your username
-                     passwd="cookies",  # your password
-                     db="hacknc")        # name of the data base
+def get_user(user, password):
+    db = MySQLdb.connect(host="localhost",    # your host, usually localhost
+                         user="root",         # your username
+                         passwd="cookies",  # your password
+                         db="hacknc")        # name of the data base
 
-# you must create a Cursor object. It will let
-#  you execute all the queries you need
-cur = db.cursor()
+    # you must create a Cursor object. It will let
+    #  you execute all the queries you need
+    cur = db.cursor()
 
-# Use all the SQL you like
-cur.execute("SELECT NesID FROM auth")
+    # Use all the SQL you like
+    cur.execute("SELECT NesID FROM auth")
 
-# print all the first cell of all the rows
-for row in cur.fetchall():
-    print row[0]
+    # print all the first cell of all the rows
+    rtn = ""
+    for row in cur.fetchall():
+        rtn = row[0]
 
-db.close()
+    db.close()
+    print(rtn)
+    return rtn
+
+if __name__ == "__main__":
+    get_user("", "")
