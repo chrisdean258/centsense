4b12661a4cbd642ef68550c765b0e2ad75e52b75
chrisdean258 <chrisdean258@gmail.com>
2017-11-04 23:38:23 -0400

    first working "server"

--- server.py
+++ server.py
@@ -1,11 +1,10 @@
 from flask import Flask
 from flask import send_file
+from flask import request
+
 app = Flask(__name__)
 
-
-@app.route('/test')
-def charts():
-    return send_file('chart.html')
+server = 'localhost'
 
 @app.route('/static')
 def stat():
@@ -15,13 +14,26 @@ def stat():
 def Chartjs():
     return send_file('JS_Dependencies/Chart.bundle.min.js')
 
+@app.route("/JS_Dependencies/StarAdmin-Free-Bootstrap-Admin-Template-master/css/style.css")
+def css():
+    return send_file('JS_Dependencies/StarAdmin-Free-Bootstrap-Admin-Template-master/css/style.css')
+
+@app.route('/')
+def index():
+    return send_file('index.html')
+
 @app.route('/hello')
 def hello_world():
     return 'Hello World!'
 
-@app.route("/JS_Dependencies/StarAdmin-Free-Bootstrap-Admin-Template-master/css/style.css")
-def css():
-    return send_file('JS_Dependencies/StarAdmin-Free-Bootstrap-Admin-Template-master/css/style.css')
+@app.route('/login', methods = ['POST'])
+def POST_login():
+    print(request.values)
+    return send_file('static.html')
+
+
+
+
 
 if __name__ == '__main__':
     app.run()
